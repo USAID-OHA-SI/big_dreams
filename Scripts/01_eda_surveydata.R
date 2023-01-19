79dca36a9e652ee71600372e44b0e1e9530a16eb
jhoehner <jhoehner@usaid.gov>
2023-01-19 14:56:14 -0500

    working through initial descriptive analysis

--- Scripts/01_eda_surveydata.R
+++ Scripts/01_eda_surveydata.R
@@ -4,7 +4,7 @@
 # REF ID:   0ffffad3 
 # LICENSE:  MIT
 # DATE:     2022-12-08
-# UPDATED: 
+# UPDATED:  2023-01-04
 
 # DEPENDENCIES ----------------------------------------------------------------
   
@@ -18,6 +18,7 @@
   library(lattice)
   library(ggplot2)
   library(GGally)
+  library(googlesheets4)
 
 # GLOBAL VARIABLES ------------------------------------------------------------
   
@@ -29,77 +30,188 @@
 
 # IMPORT ----------------------------------------------------------------------
 
-  path <- "Data/finaldataset.csv"
-  
-  df <- read_csv(path, show_col_types = FALSE)
+ df <-  read_sheet("1GdrrRY3Ebhb_My0UWgRHyOx_muk5cyrGdqFUwmt4KQQ")
 
 # MUNGE -----------------------------------------------------------------------
-  
-  skim_results <- clean_df %>%skim()
-  
-  clean_df <- df %>%
+       
+  total_agyw <- df %>%
     clean_names() %>%
+    select(country, householdid, personid, centroidid, region,
+           intwt0, hivstatusfinal, agegroup, outofschool, orphanhood,
+           pregnancyhistory, transactional, alcuse, sti_all,
+           sexpartners, irregular, sexualviolence, adattck, eversex_1014) %>%
+    # do we have data on condom use for 15-19 yr olds or 20-24 yr olds? 
+    # This is a risk factor for these groups in SA's methods and I'm not 
+    # seeing it in the data
     mutate(
-      hivstatusfinal = na_if(hivstatusfinal, 99), 
-      hivstatusfinal = if_else(hivstatusfinal == 2, 0, hivstatusfinal)) %>%
-    # cannot include, all obs are missing
-    select(country, personid, centroidid, 
-           intwt0, hivstatusfinal, age, school, 
-           pregnancy, transactional, 
-           sexpartners, irregular, sexviolence)
+      hivstatusfinal = na_if(hivstatusfinal, 99),
+      # create "any_risk" wherein an individual has any one or more of the risk factors 
+      # for their age group
+      # 1= 10-14 years
+      # 2= 15-19 years
+      # 3= 20-24 years
+      # 4= 25-29 years
+      any_risk_1014 = if_else((agegroup == 1) & 
+                              (eversex_1014 == 1 |
+                              pregnancyhistory == 1 |
+                              sexualviolence == 1 |
+                              adattck == 1 |
+                              alcuse == 1 |
+                              outofschool == 1|
+                              orphanhood == 1), 
+                              1, 0),
+      # is adattck not a risk factor for 15-19 or 20-24 yr olds?
+      # i am seeing it in SA's methods for 10-14 yr olds only
+      any_risk_1519 = if_else((agegroup == 2) &
+                              (sexpartners == 1 |
+                              pregnancyhistory == 1 | 
+                              sti_all == 1 |
+                              irregular == 1 |
+                              transactional == 1 | 
+                              sexualviolence == 1 |
+                              alcuse == 1 |
+                              outofschool == 1|
+                              orphanhood == 1), 
+                              1, 0),
+      any_risk_2024 = if_else((agegroup == 3) &
+                              (sexpartners == 1 |
+                              pregnancyhistory == 1 | 
+                              sti_all == 1 |
+                              irregular == 1 |
+                              transactional == 1 | 
+                              sexualviolence == 1 |
+                              alcuse == 1), 
+                              1, 0)) 
   
-  age_pop <- clean_df %>%
-    select(country, age) %>%
-    mutate(
-      age = as.numeric(age)) %>%
-    filter(age > 14 & age < 25) %>%
-    mutate(
-      yi = sum(nrow(.)),
-      sd = sd(age), 
-      cv = raster::cv(age))
+  # Check for completeness of important vars
   
-  # Do I have the data I need for a Simple area-level model
+  skim_results <- total_agyw %>% skim()
   
-  # yi = number of agyw 15-24
-  # sd = std. dev of yi
-  # cv = coef. of var for yi
-  # SmallArea = areas of interest (ideally DSNU, likely PSNU or SNU)
-  # ni = small area sample sizes  (ideally, DSNU, likely PSNU or SNU sample sizes)
-  # MajorArea = major areas containing the smaller areas of interest (ideally, district, likely OU)
+  # 1096 participants (24%) missing HIV status
+  # this is consistent with SA's experience
   
-  model <- df$yi ~ as.factor(df$MajorArea)
+  # does it vary across ages?
   
-  # Mean Squared Error estimator
-  df_fh_model_mse <- mseFH(model, df$SD^2)
+  tabyl(total_agyw, agegroup, hivstatusfinal)
   
-  # create the dataframe with results
-  df_results_df <- data.frame(Area = df$SmallArea, 
-                                SampleSize = df$ni, 
-                                DIR = df$yi, 
-                                cv.DIR = 100 * df$CV, 
-                                eblup.FH = df_fh_model_mse$est$eblup) %>%
-    mutate(
-      # CV estimator for MSE estimator 
-      df_cv_fh = as.numeric(100 * sqrt(df_fh_model_mse$mse) / df_fh_model_mse$est$eblup)) %>%
-    arrange(-SampleSize) %>%
-    pivot_longer(cols = c(DIR, eblup.FH, cv.DIR, df_cv_fh),
-                 names_to = "estimator_type", 
-                 values_to = "value")
+  # agegroup   1   2 NA_
+  # 1         20 569 673
+  # 2         74 979 138
+  # 3        205 717 153
+  # 4        326 497 132
   
-  # Can we use the survey response data?
+  # yes, most who are missing this are between ages 10-14
+  # note: most who are positive are in age group 4, 25-29
+  # it looks like age is correlated with HIV status
   
-  # what kind of missing data do we have?
-  # are data missing at random?
-  # if we can safely assume data are MAR (TBD!)
-  # we would proceed to select which to include/impute
+  ggplot(total_agyw, aes(agegroup, hivstatusfinal)) +
+    geom_point()
   
-  # skim_results <- clean_df%>%
-  #   skim() %>%
-  #   select(skim_variable, n_missing, complete_rate) %>%
-  #   arrange(complete_rate)
-  # 
-  # skim_results_2 <- skim_results %>%
-  #   filter(complete_rate > 0)
+  # How many are missing all risk factors for their age group?
+  
+  missing_all_rfs <- total_agyw %>%
+    filter((agegroup == 1 &
+               is.na(outofschool) & 
+               is.na(orphanhood) & 
+               is.na(pregnancyhistory) & 
+               is.na(transactional) & 
+               is.na(alcuse) &
+               is.na(sti_all) &
+               is.na(sexpartners) &
+               is.na(irregular) &
+               is.na(sexualviolence) &
+               is.na(adattck) &
+               is.na(eversex_1014)) |
+            (agegroup == 2 & 
+              is.na(outofschool) & 
+              is.na(orphanhood) & 
+              is.na(pregnancyhistory) & 
+              is.na(transactional) & 
+              is.na(alcuse) &
+              is.na(sti_all) &
+              is.na(sexpartners) &
+              is.na(irregular) &
+              is.na(sexualviolence)) |
+           (agegroup == 3 &
+              is.na(transactional) & 
+              is.na(alcuse) &
+              is.na(sti_all) &
+              is.na(sexpartners) &
+              is.na(irregular) &
+              is.na(sexualviolence)))
+  
+  # 857 (19%) participants missing all risk factors 
+  
+  # does this vary by age?
+  
+  tabyl(missing_all_rfs, agegroup)
+  
+  # agegroup   n   percent
+  # 1         658 0.7677946
+  # 2         102 0.1190198
+  # 3          97 0.1131855
+  
+  # yes, most (76%) of those missing all RFs for their age are in the 10-14 age group
+
+  # filter out those missing HIV status or all risk factors for their age
+  total_agyw_known <- total_agyw %>%
+    drop_na(hivstatusfinal) %>%
+    filter(!personid %in% missing_all_rfs$personid, 
+           # filtering out oldest group since i'm using SA's methods
+           # and they don't have oldest group RFs
+           agegroup != 4)
+  
+  hiv_pos_agyw <- total_agyw_known %>%
+    filter(hivstatusfinal == 1)
+  
+  # overall
+  hiv_pos_overall <- (nrow(hiv_pos_agyw)/nrow(total_agyw_known)) * 100
+  
+  # 10-14
+  hiv_pos_agyw_1 <- total_agyw_known %>%
+    filter(hivstatusfinal == 1 & agegroup == 1)
+  
+  total_agyw_1 <- total_agyw_known %>%
+    filter(agegroup == 1)
+  
+  hiv_pos_1 <- (nrow(hiv_pos_agyw_1)/nrow(total_agyw_1)) * 100
+  
+  # 15-19
+  hiv_pos_2 <- total_agyw_known %>%
+    filter(hivstatusfinal == 1 & agegroup == 2)
+  
+  total_agyw_2 <- total_agyw_known %>%
+    filter(agegroup == 2)
+  
+  hiv_pos_2 <- (nrow(hiv_pos_2)/nrow(total_agyw_2)) * 100
+  
+  # 20-24
+  
+  hiv_pos_3 <- total_agyw_known %>%
+    filter(hivstatusfinal == 1 & agegroup == 3)
+  
+  total_agyw_3 <- total_agyw_known %>%
+    filter(agegroup == 3)
+  
+  hiv_pos_3 <- (nrow(hiv_pos_3)/nrow(total_agyw_3)) * 100
+          
+  # We know the HIV status of 76% of our sample, is this stil a sufficient sample?
+  
+  hiv_neg_atrisk <- total_agyw_known %>%
+    filter(hivstatusfinal == 2 & 
+           any_risk_1014 == 1 |
+           any_risk_1519 == 1 |
+           any_risk_2024 == 1)
+  
+  # population most vulnerable to HIV acquisition
+  
+  total_AGYW_atrisk = round((nrow(total_agyw_known) - nrow(hiv_pos_agyw)) * (sum(nrow(hiv_neg_atrisk)/nrow(total_agyw_known))), 0)
+  
+  # 1762 
+ 
+  # Correlation plot
+  # outcome = number of at-risk AGYW in DREAMS SNUs
+  # number of AGYW ages 10-24 with one or more risk factors
   
   
  
